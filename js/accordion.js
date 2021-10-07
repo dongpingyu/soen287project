91437865435ab1c3d82a29345a85b34af24cb609
bentrd <benjamin@tordjman.com>
2021-10-07 00:44:01 -0400

    aisles now pre-opened

--- js/accordion.js
+++ js/accordion.js
@@ -1,14 +1,14 @@
-var acc = document.getElementsByClassName("accordion");
+var acc = document.getElementsByClassName("accordion active");
 var i;
 
 for (i = 0; i < acc.length; i++) {
   acc[i].addEventListener("click", function() {
     this.classList.toggle("active");
     var product = this.nextElementSibling;
-    if (product.style.display === "block") {
-      product.style.display = "none";
-    } else {
+    if (product.style.display === "none") {
       product.style.display = "block";
+    } else {
+      product.style.display = "none";
     }
   });
 }
